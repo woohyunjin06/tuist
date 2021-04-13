fb83078580fa3ccc5de37ef1eea20a98702094cd
fortmarek <marekfort@me.com>
2021-04-13 08:47:32 +0200

    Create Tasks model

--- Sources/TuistLoader/Loaders/ManifestLoader.swift
+++ Sources/TuistLoader/Loaders/ManifestLoader.swift
@@ -86,6 +86,11 @@ public protocol ManifestLoading {
     /// - Parameters:
     ///     -  path: Path to the directory that contains Dependencies.swift
     func loadDependencies(at path: AbsolutePath) throws -> ProjectDescription.Dependencies
+    
+    /// Loads the Tasks.swift in the given directory
+    /// - Parameters:
+    ///     - path: Path to the directory that contains Tasks.swift
+    func loadTasks(at path: AbsolutePath) throws -> ProjectDescription.Tasks
 
     /// Loads the Plugin.swift in the given directory.
     /// - Parameter path: Path to the directory that contains Plugin.swift
@@ -255,7 +260,8 @@ public class ManifestLoader: ManifestLoading {
                  .project,
                  .setup,
                  .template,
-                 .workspace:
+                 .workspace,
+                 .tasks:
                 return try projectDescriptionHelpersBuilder.build(
                     at: path,
                     projectDescriptionSearchPaths: searchPaths,
